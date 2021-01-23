cd757502a354301fa75f4fc009c2001450a8e9c8
ceruleanox <Ceruleanox@gmail.com>
2021-01-23 00:45:26 -0500

    built chat skeleton in room.html; integrated socket.io to broadcast when user enters or leaves room

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Run when client connects
+io.on('connection', socket => {
+    console.log("Establishing new WebSocket connection...");
+
+    // automatic message upon loading or reloading page 
+    socket.emit('message', 'Welcome to Yoga Remastered: C&C!');
+
+    // broadcast when a user connects
+    // does not broadcast to user entering room
+    socket.broadcast.emit('message', 'A user has joined the chat');
+
+    // runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+});
+
 const PORT = 3000 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
