d8cbf266752502b83ac124e40bc0bd7c7a83bca1
Prahitha <44160152+Prahitha@users.noreply.github.com>
2021-01-23 21:46:12 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
-const { userJoin, getCurrentUser } = require('./utils/users');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
 
 const app = express();
 const server = http.createServer(app);
@@ -40,18 +40,35 @@ io.on('connection', socket => {
             "message",
             formatMessage(botName, `${user.username} has joined the room`)
     );
+
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
   });
 
   // listen for chat message
   socket.on("chatMessage", (msg) => {
-    io.emit("message", formatMessage("USER", msg));
+    const user = getCurrentUser(socket.id);
 
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+  
     // run when client disconnects
     socket.on("disconnect", () => {
-      io.emit("message", formatMessage(botName, "A user has left the room"));
+      const user = userLeave(socket.id);
+
+      if (user){
+        io.to(user.room)
+        .emit("message", formatMessage(botName, `${user.username} has left the room`));
+
+        io.to(user.room).emit('roomUsers', {
+          room: user.room,
+          users: getRoomUsers(user.room)
+        });
+      }
     });
   });
-});
 
 const PORT = 3000 || process.env.PORT;
 
