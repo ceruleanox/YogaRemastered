1083dfc5dbbf7f8a3f98da94d0bf73abf1f279e4
Prahitha <44160152+Prahitha@users.noreply.github.com>
2021-01-23 21:47:22 +0530

    Add files via upload

--- utils/users.js
+++ utils/users.js
@@ -14,7 +14,23 @@ function getCurrentUser(id) {
     return users.find(user => user.id === id);
 }
 
+// user leaves the chat  
+function userLeave(id) {
+    const index = users.findIndex(user => user.id === id);
+  
+    if (index !== -1) {
+      return users.splice(index, 1)[0];
+    }
+}
+
+// get users in the room
+function getRoomUsers(room) {
+    return users.filter(user => user.room === room);
+}
+
 module.exports = {
     userJoin,
-    getCurrentUser
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
 };
